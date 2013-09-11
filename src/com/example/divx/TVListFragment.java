49a1d79870078a8720911399f03064825406e9b4
Abhishek Sharma <abhishek@snapstick.com>
2013-09-11 15:26:12 +0530

    Adding dial client code

--- src/com/example/divx/TVListFragment.java
+++ src/com/example/divx/TVListFragment.java
@@ -1,5 +1,8 @@
 package com.example.divx;
 
+import java.io.IOException;
+import java.util.List;
+
 import android.app.AlertDialog;
 import android.app.Dialog;
 import android.app.DialogFragment;
@@ -8,11 +11,28 @@ import android.os.Bundle;
 import android.util.Log;
 import android.widget.ArrayAdapter;
 
+import com.snapstick.dial.client.Device;
+import com.snapstick.dial.client.DialClient;
+
 public class TVListFragment extends DialogFragment {
 	@Override
 	public Dialog onCreateDialog(Bundle savedInstanceState) {
-		String[] devices = new String[]{"Kunals-TV", "Abhisheks-TV"};
-		ArrayAdapter<String> adapter = new ArrayAdapter<String>(getActivity(), android.R.layout.simple_list_item_1, devices);
+		List<Device> devices;
+		try {
+			devices = DialClient.getDeviceList();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			devices = null;
+		}
+		
+		int i = 0;
+		String[] names = new String[devices.size()];
+		for(Device d: devices) {
+			names[i] = d.getName();
+			i++;
+		}
+		ArrayAdapter<String> adapter = new ArrayAdapter<String>(getActivity(), android.R.layout.simple_list_item_1, names);
 	    AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
 	    builder.setTitle("TVs near you");
 	    builder.setAdapter(adapter, new DialogInterface.OnClickListener() {
